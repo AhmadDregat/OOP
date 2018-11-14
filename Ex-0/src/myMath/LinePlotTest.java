38c688505f13da9637d6aa9e54dee0ad42a24278
ahmaddregat <ahmad.dregat.97@hotmail.com>
2018-11-14 22:08:13 +0200

    ....

--- Ex-0/src/myMath/LinePlotTest.java
+++ Ex-0/src/myMath/LinePlotTest.java
@@ -1,4 +1,8 @@
 package myMath;
+/**
+ * The path from which we took the code:https://github.com/eseifert/gral.git
+ * 
+ */
 import java.awt.Color;
 import javax.swing.JFrame;
 import de.erichseifert.gral.data.DataTable;
@@ -8,17 +12,19 @@ import de.erichseifert.gral.plots.lines.LineRenderer;
 import de.erichseifert.gral.plots.points.PointRenderer;
 import de.erichseifert.gral.ui.InteractivePanel;
 
-	
+
 public class LinePlotTest extends JFrame {
-	 
+
 	public LinePlotTest() {
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(600, 400);
-		Polynom p = new Polynom("2x^2");
+		Polynom p1=new Polynom();
+		p1.add(new Monom(1,2));
+		p1.add(new Monom(5,0));
 		DataTable data = new DataTable(Double.class, Double.class);
 		for (double x = -5.0; x <= 5.0; x+=0.25) {
-			double y = p.f(x);
+			double y = p1.f(x);
 			data.add(x, y);
 		}
 		XYPlot plot = new XYPlot(data);
